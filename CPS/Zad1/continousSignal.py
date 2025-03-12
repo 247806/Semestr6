887296a359cfb68c964a23695c728bde7d1fa248
247659 <247659@edu.p.lodz.pl>
2025-03-12 21:33:14 +0100

    CPS -> continus signal done

--- CPS/Zad1/continousSignal.py
+++ CPS/Zad1/continousSignal.py
@@ -1,28 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def sinusoidal(A, T, t1, d):
-    sample_rate = 1000
-    time = np.arange(t1, d, 1 / sample_rate)
+def random_uniform_signal(A, t1, d, time):
+    signal = np.random.uniform(-A, A, size=len(time))
+    return signal
+
+def gaussian_noise(A, t1, d, time):
+    signal = A * np.random.normal(loc=0, scale=1, size=len(time))
+    return signal
+
+def sinusoidal(A, T, t1, d, time):
     signal = A * np.sin((2 * np.pi / T) * time)
-    return time, signal
+    return signal
 
-def halfWaveSinusoidal(A, T, t1, d):
-    sample_rate = 1000
-    time = np.arange(t1, d, 1 / sample_rate)
+def halfWaveSinusoidal(A, T, t1, d, time):
     signal = 0.5 * A * (np.sin((2 * np.pi / T) * time) + np.abs(np.sin((2 * np.pi / T) * time)))
-    return time, signal
+    return signal
 
-def halfSinusoidal(A, T, t1, d):
-    sample_rate = 1000
-    time = np.arange(t1, d, 1 / sample_rate)
+def halfSinusoidal(A, T, t1, d, time):
     signal = A * np.abs(np.sin((2 * np.pi / T) * time))
-    return time, signal
+    return signal
 
 
-def square(A, T, t1, d, kw):
-    sample_rate = 1000
-    time = np.arange(t1, d, 1 / sample_rate)
+def square(A, T, t1, d, kw, time):
     signal = np.zeros(len(time))
     k = 0
     i = 0
@@ -35,11 +35,9 @@ def square(A, T, t1, d, kw):
             k += 1
         i += 1
 
-    return time, signal
+    return signal
 
-def squareSymetric(A, T, t1, d, kw):
-    sample_rate = 1000
-    time = np.arange(t1, d, 1 / sample_rate)
+def squareSymetric(A, T, t1, d, kw, time):
     signal = np.zeros(len(time))
     k = 0
     i = 0
@@ -52,11 +50,9 @@ def squareSymetric(A, T, t1, d, kw):
             k += 1
         i += 1
 
-    return time, signal
+    return signal
 
-def triangle(A, T, t1, d, kw):
-    sample_rate = 1000
-    time = np.arange(t1, d, 1 / sample_rate)
+def triangle(A, T, t1, d, kw, time):
     signal = np.zeros(len(time))
     k = 0
     i = 0
@@ -69,11 +65,9 @@ def triangle(A, T, t1, d, kw):
             k += 1
         i += 1
 
-    return time, signal
+    return signal
 
-def ones(A, t1, d, ts):
-    sample_rate = 1000
-    time = np.arange(t1, d, 1 / sample_rate)
+def ones(A, t1, d, ts, time):
     signal = np.zeros(len(time))
     i = 0
     for times in time:
@@ -85,20 +79,4 @@ def ones(A, t1, d, ts):
             signal[i] = 0
         i += 1
 
-    return time, signal
-
-
-
-def plot_signal(time, signal, signal_type):
-    plt.figure()
-    plt.plot(time, signal)
-    plt.xlabel("Time [s]")
-    plt.ylabel("Amplitude")
-    plt.title(f"{signal_type.capitalize()} Signal")
-    plt.grid()
-    plt.show()
-
-
-if __name__ == '__main__':
-    time, signal = triangle(A=10, T =2,  t1=0, d=10, kw=0.5)
-    plot_signal(time, signal, 'sinusoidal')
+    return signal
--- CPS/Zad1/front.py
+++ CPS/Zad1/front.py
@@ -3,13 +3,13 @@ from tkinter import ttk
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import constant
+import continousSignal
 
 
 # Funkcja do generowania wykresu w aplikacji
 def generate_signal():
     A = float(amplitude_entry.get())
-    T = float(duty_cycle_entry_t.get()) if signal_type.get() != "ones" else None
+    T = float(duty_cycle_entry_t.get()) if signal_type.get() != "ones" and signal_type.get() != "random_uniform_signal" and signal_type.get() != "gaussian_noise" else None
     t1 = float(start_time_entry.get())
     d = float(duration_entry.get())
 
@@ -17,20 +17,27 @@ def generate_signal():
     kw = float(duty_cycle_entry.get()) if signal_type.get() == "squareSymmetric" or signal_type.get() == "square" or signal_type.get() == "triangle" else None
     ts = float(duty_cycle_entry_ts.get()) if signal_type.get() == "ones" else None
 
+    sample_rate = float(sample_rate_entry.get())
+    time = np.arange(t1, d, 1 / sample_rate)
+
     if signal_type.get() == "sinusoidal":
-        time, signal = constant.sinusoidal(A, T, t1, d)
+        signal = constant.sinusoidal(A, T, t1, d, time)
     elif signal_type.get() == "squareSymmetric":
-        time, signal = constant.squareSymetric(A, T, t1, d, kw)
+        signal = constant.squareSymetric(A, T, t1, d, kw, time)
     elif signal_type.get() == 'halfWaveSinusoidal':
-        time, signal = constant.halfWaveSinusoidal(A, T, t1, d)
+        signal = constant.halfWaveSinusoidal(A, T, t1, d, time)
     elif signal_type.get() == 'halfSinusoidal':
-        time, signal = constant.halfSinusoidal(A, T, t1, d)
+        signal = constant.halfSinusoidal(A, T, t1, d, time)
     elif signal_type.get() == 'square':
-        time, signal = constant.square(A, T, t1, d, kw)
+        signal = constant.square(A, T, t1, d, kw, time)
     elif signal_type.get() == 'triangle':
-        time, signal = constant.triangle(A, T, t1, d, kw)
+        signal = constant.triangle(A, T, t1, d, kw, time)
     elif signal_type.get() == 'ones':
-        time, signal = constant.ones(A, t1, d, ts)
+        signal = constant.ones(A, t1, d, ts, time)
+    elif signal_type.get() == 'random_uniform_signal':
+        signal = constant.random_uniform_signal(A, t1, d, time)
+    elif signal_type.get() == 'gaussian_noise':
+        signal = constant.gaussian_noise(A, t1, d, time)
 
     plot_signal(time, signal, signal_type.get())
 
@@ -61,52 +68,63 @@ def plot_signal(time, signal, signal_type):
 # Funkcja do pokazywania/ukrywania pola dla współczynnika wypełnienia
 def toggle_fields():
     if signal_type.get() in ["squareSymmetric", "square", "triangle"]:
-        duty_cycle_label.grid(row=5, column=0, padx=5, pady=5)
-        duty_cycle_entry.grid(row=5, column=1, padx=5, pady=5)
+        duty_cycle_label.grid(row=6, column=0, padx=5, pady=5)
+        duty_cycle_entry.grid(row=6, column=1, padx=5, pady=5)
     else:
         duty_cycle_label.grid_remove()
         duty_cycle_entry.grid_remove()
 
     if signal_type.get() == "ones":
-        duty_cycle_label_ts.grid(row=5, column=0, padx=5, pady=5)
-        duty_cycle_entry_ts.grid(row=5, column=1, padx=5, pady=5)
-        duty_cycle_label_t.grid_remove()
-        duty_cycle_entry_t.grid_remove()
+        duty_cycle_label_ts.grid(row=6, column=0, padx=5, pady=5)
+        duty_cycle_entry_ts.grid(row=6, column=1, padx=5, pady=5)
     else:
         duty_cycle_label_ts.grid_remove()
         duty_cycle_entry_ts.grid_remove()
-        duty_cycle_label_t.grid(row=1, column=0, padx=5, pady=5)
-        duty_cycle_entry_t.grid(row=1, column=1, padx=5, pady=5)
 
+
+    if signal_type.get() == "random_uniform_signal" or signal_type.get() == "ones" or signal_type.get() == "gaussian_noise":
+        duty_cycle_label_t.grid_remove()
+        duty_cycle_entry_t.grid_remove()
+    else:
+        duty_cycle_label_t.grid(row=3, column=0, padx=5, pady=5)
+        duty_cycle_entry_t.grid(row=3, column=1, padx=5, pady=5)
 
 # Tworzenie głównego okna aplikacji
 root = tk.Tk()
 root.title("Generator sygnałów")
 
 # Etykiety i pola do wprowadzania danych
-ttk.Label(root, text="Amplituda (A):").grid(row=0, column=0, padx=5, pady=5)
+# Opcja wyboru typu sygnału
+ttk.Label(root, text="Typ sygnału:").grid(row=0, column=0, padx=5, pady=5)
+signal_type = tk.StringVar(value="sinusoidal")
+signal_dropdown = ttk.Combobox(root, textvariable=signal_type, values=["random_uniform_signal", "gaussian_noise", "sinusoidal", "squareSymmetric", "halfWaveSinusoidal", "halfSinusoidal", "square", "triangle",  "ones"],
+                               state="readonly")
+signal_dropdown.grid(row=0, column=1, padx=5, pady=5)
+signal_dropdown.bind("<<ComboboxSelected>>", lambda e: toggle_fields())  # Aktualizacja przy zmianie wyboru
+
+sample_rate_var = tk.StringVar(value="1000")  # Ustaw domyślną wartość np. 1000 Hz
+
+ttk.Label(root, text="Częstotliwość próbkowania").grid(row=1, column=0, padx=5, pady=5)
+sample_rate_entry = ttk.Entry(root, textvariable=sample_rate_var)  # Przypisanie zmiennej
+sample_rate_entry.grid(row=1, column=1, padx=5, pady=5)
+
+ttk.Label(root, text="Amplituda (A):").grid(row=2, column=0, padx=5, pady=5)
 amplitude_entry = ttk.Entry(root)
-amplitude_entry.grid(row=0, column=1, padx=5, pady=5)
+amplitude_entry.grid(row=2, column=1, padx=5, pady=5)
 
 # ttk.Label(root, text="Okres (T):").grid(row=1, column=0, padx=5, pady=5)
 # period_entry = ttk.Entry(root)
 # period_entry.grid(row=1, column=1, padx=5, pady=5)
 
-ttk.Label(root, text="Czas początkowy (t1):").grid(row=2, column=0, padx=5, pady=5)
+ttk.Label(root, text="Czas początkowy (t1):").grid(row=4, column=0, padx=5, pady=5)
 start_time_entry = ttk.Entry(root)
-start_time_entry.grid(row=2, column=1, padx=5, pady=5)
+start_time_entry.grid(row=4, column=1, padx=5, pady=5)
 
-ttk.Label(root, text="Czas trwania (d):").grid(row=3, column=0, padx=5, pady=5)
+ttk.Label(root, text="Czas trwania (d):").grid(row=5, column=0, padx=5, pady=5)
 duration_entry = ttk.Entry(root)
-duration_entry.grid(row=3, column=1, padx=5, pady=5)
+duration_entry.grid(row=5, column=1, padx=5, pady=5)
 
-# Opcja wyboru typu sygnału
-ttk.Label(root, text="Typ sygnału:").grid(row=4, column=0, padx=5, pady=5)
-signal_type = tk.StringVar(value="sinusoidal")
-signal_dropdown = ttk.Combobox(root, textvariable=signal_type, values=["sinusoidal", "squareSymmetric", "halfWaveSinusoidal", "halfSinusoidal", "square", "triangle", "ones"],
-                               state="readonly")
-signal_dropdown.grid(row=4, column=1, padx=5, pady=5)
-signal_dropdown.bind("<<ComboboxSelected>>", lambda e: toggle_fields())  # Aktualizacja przy zmianie wyboru
+
 
 # Pola dla współczynnika wypełnienia i czasu skoku (ukryte domyślnie)
 duty_cycle_label = ttk.Label(root, text="Współczynnik wypełnienia (kw):")
@@ -120,11 +138,11 @@ duty_cycle_entry_t = ttk.Entry(root)
 
 # Przycisk do generowania wykresu
 generate_button = ttk.Button(root, text="Generuj sygnał", command=generate_signal)
-generate_button.grid(row=6, column=0, columnspan=2, pady=10)
+generate_button.grid(row=7, column=0, columnspan=2, pady=10)
 
 # Ramka do osadzenia wykresu
 plot_frame = ttk.Frame(root)
-plot_frame.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+plot_frame.grid(row=8, column=0, columnspan=2, padx=10, pady=10)
 
 # Ukrycie pola na starcie (bo domyślnie jest sinusoidal)
 toggle_fields()
