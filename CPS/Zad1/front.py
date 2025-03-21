615f28941b3be39edb84e7cf19acd02b86fad10c
247659 <247659@edu.p.lodz.pl>
2025-03-21 02:29:21 +0100

    CPS -> Save and Load added

--- CPS/Zad1/front.py
+++ CPS/Zad1/front.py
@@ -1,5 +1,6 @@
+import struct
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, filedialog
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -7,6 +8,7 @@ import continousSignal
 import discretSignal
 import calculateParams as cp
 import signalOperation as so
+
 signal_1 = None
 time_1 = None
 signal_2 = None
@@ -73,7 +75,7 @@ def generate_signal():
         d = full_periods * T
         time_1, signal_1 = function_type(A, T, t1, d, kw, ts, p, signal_1)
 
-def plot_signal(time, signal, signal_type, plot, histogram):
+def plot_signal(time, signal, signal_types, plot, histogram):
 
     for widget in plot.winfo_children():
         widget.destroy()
@@ -82,7 +84,7 @@ def plot_signal(time, signal, signal_type, plot, histogram):
         widget.destroy()
 
     fig, ax = plt.subplots()
-    if signal_type in ["Impuls jednostkowy", "Szum impulsowy"]:
+    if signal_types in ["Impuls jednostkowy", "Szum impulsowy"]:
         ax.scatter(time, signal)
     else:
         ax.plot(time, signal)
@@ -93,6 +95,14 @@ def plot_signal(time, signal, signal_type, plot, histogram):
     canvas = FigureCanvasTkAgg(fig, master=plot)
     canvas.draw()
     canvas.get_tk_widget().pack(expand=True, fill='both', padx=5, pady=5)
+
+def histogram_managment():
+    if signal_notebook.index(signal_notebook.select()) == 0:
+        plot_histogram(histogram_frame_1, signal_1)
+    elif signal_notebook.index(signal_notebook.select()) == 1:
+        plot_histogram(histogram_frame_2, signal_2)
+    else:
+        plot_histogram(histogram_frame_3, signal_3)
 
 def plot_histogram(frame, signal):
     for widget in frame.winfo_children():
@@ -141,35 +151,35 @@ def toggle_fields():
 def create_parameters_tab(param, signal, time):
     if signal_type.get() not in ["Impuls jednostkowy", "Szum impulsowy"]:
         avg_label = ttk.Label(param, text=f"Średnia: {cp.avg_cont(signal, time):.2f}")
-        avg_label.grid(row=0, column=0, padx=5, pady=5)
+        avg_label.pack(padx=5, pady=5)
 
         abs_avg_label = ttk.Label(param, text=f"Średnia bezwzględna: {cp.abs_avg_cont(signal, time):.2f}")
-        abs_avg_label.grid(row=1, column=0, padx=5, pady=5)
+        abs_avg_label.pack(padx=5, pady=5)
 
         power_label = ttk.Label(param, text=f"Moc: {cp.power_cont(signal, time):.2f}")
-        power_label.grid(row=2, column=0, padx=5, pady=5)
+        power_label.pack(padx=5, pady=5)
 
         dev_label = ttk.Label(param, text=f"Wariancja: {cp.dev_cont(signal, time):.2f}")
-        dev_label.grid(row=3, column=0, padx=5, pady=5)
+        dev_label.pack(padx=5, pady=5)
 
         eff_power_label = ttk.Label(param, text=f"Wartość skuteczna: {cp.eff_power_cont(signal, time):.2f}")
-        eff_power_label.grid(row=4, column=0, padx=5, pady=5)
+        eff_power_label.pack(padx=5, pady=5)
 
     else:
         avg_label = ttk.Label(param, text=f"Średnia: {cp.avg_dis(signal, time):.2f}")
-        avg_label.grid(row=0, column=0, padx=5, pady=5)
+        avg_label.pack(padx=5, pady=5)
 
         abs_avg_label = ttk.Label(param, text=f"Średnia bezwzględna: {cp.abs_avg_dis(signal, time):.2f}")
-        abs_avg_label.grid(row=1, column=0, padx=5, pady=5)
+        abs_avg_label.pack(padx=5, pady=5)
 
         power_label = ttk.Label(param, text=f"Moc: {cp.power_dis(signal, time):.2f}")
-        power_label.grid(row=2, column=0, padx=5, pady=5)
+        power_label.pack(padx=5, pady=5)
 
         dev_label = ttk.Label(param, text=f"Wariancja: {cp.dev_dis(signal, time):.2f}")
-        dev_label.grid(row=3, column=0, padx=5, pady=5)
+        dev_label.pack(padx=5, pady=5)
 
         eff_power_label = ttk.Label(param, text=f"Wartość skuteczna: {cp.eff_power_dis(signal, time):.2f}")
-        eff_power_label.grid(row=4, column=0, padx=5, pady=5)
+        eff_power_label.pack(padx=5, pady=5)
 
 def plot_empty_chart():
     fig, ax = plt.subplots()
@@ -215,9 +225,122 @@ def operate_signals(operation):
     plot_histogram(histogram_frame_3, signal_3)
     create_parameters_tab(param_frame_3, signal_3, time_3)
 
+def save_signal():
+    global signal_1, time_1
+    if signal_notebook.index(signal_notebook.select()) == 0:
+        signal = signal_1
+        time = time_1
+    elif signal_notebook.index(signal_notebook.select()) == 1:
+        signal = signal_2
+        time = time_2
+    else:
+        signal = signal_3
+        time = time_3
+
+    if time is None or signal is None:
+        print("Brak danych do zapisania")
+        return
+
+    if len(time) != len(signal):
+        print("Długości time i signal muszą się zgadzać")
+        return
+
+    start_time = time[0]
+
+    sampling_rate = 1 / (time[1] - time[0]) if len(time) > 1 else 1.0
+
+    num_samples = len(signal)
+
+    max_amplitude = max(abs(s) for s in signal)
+
+    file_path = filedialog.asksaveasfilename(defaultextension=".bin",
+                                             filetypes=[("Binary Files", "*.bin"), ("All Files", "*.*")])
+    if not file_path:
+        return
+
+    with open(file_path, mode='wb') as file:
+        file.write(struct.pack('d', start_time))
+        file.write(struct.pack('d', sampling_rate))
+        file.write(struct.pack('i', num_samples))
+        file.write(struct.pack('d', max_amplitude))
+
+        for t, s in zip(time, signal):
+            file.write(struct.pack('d', t))
+            file.write(struct.pack('d', s))
+
+    print(f"Plik zapisany: {file_path}")
+
+def load_signal(type):
+    global time_1, signal_1, signal_2, time_2, signal_3, time_3
+    file_path = filedialog.askopenfilename(filetypes=[("Binary Files", "*.bin"), ("All Files", "*.*")])
+    if not file_path:
+        return
+
+    time_temp, signal_temp = [], []
+
+    with open(file_path, mode='rb') as file:
+        start_time = struct.unpack('d', file.read(8))[0]
+        sampling_rate = struct.unpack('d', file.read(8))[0]
+        num_samples = struct.unpack('i', file.read(4))[0]
+        max_amplitude = struct.unpack('d', file.read(8))[0]
+
+        for _ in range(num_samples):
+            t = struct.unpack('d', file.read(8))[0]  # czas
+            s = struct.unpack('d', file.read(8))[0]  # amplituda
+            time_temp.append(t)
+            signal_temp.append(s)
+
+    if signal_notebook.index(signal_notebook.select()) == 0:
+        time_1 = np.array(time_temp)
+        signal_1 = np.array(signal_temp)
+        signal = signal_1
+        time = time_1
+        plot = plot_frame_1
+        histogram = histogram_frame_1
+        param = param_frame_1
+    elif signal_notebook.index(signal_notebook.select()) == 1:
+        time_2 = np.array(time_temp)
+        signal_2 = np.array(signal_temp)
+        signal = signal_2
+        time = time_2
+        plot = plot_frame_2
+        histogram = histogram_frame_2
+        param = param_frame_2
+    else:
+        return
+
+    if type == 0:
+        plot_signal(time, signal, "Załadowany sygnał", plot, histogram)
+        create_parameters_tab(param, signal, time)
+        plot_histogram(histogram, signal)
+    else:
+        display_data_in_popup(time, signal, start_time, sampling_rate, num_samples, max_amplitude)
+
+    print(f"\nPlik wczytany: {file_path}")
+
+
+def display_data_in_popup(time_data, signal_data, start_time, sampling_rate, num_samples, max_amplitude):
+    popup = tk.Toplevel(root)
+    popup.title("Dane z pliku")
+    popup.geometry("600x400")
+
+    text_box = tk.Text(popup, wrap=tk.WORD, height=23, width=70)
+    text_box.pack(padx=10, pady=10)
+
+    text_box.insert(tk.END, f"Czas początkowy: {start_time}\n")
+    text_box.insert(tk.END, f"Częstotliwość próbkowania: {sampling_rate}\n")
+    text_box.insert(tk.END, f"Liczba próbek: {num_samples}\n")
+    text_box.insert(tk.END, f"Maksymalna amplitiuda: {max_amplitude}\n")
+    text_box.insert(tk.END, "\nCzas\tAmplituda\n")
+
+    for t, s in zip(time_data, signal_data):
+        text_box.insert(tk.END, f"{t:.6f}\t{s:.6f}\n")
+
+    text_box.config(state=tk.DISABLED)
+
 root = tk.Tk()
 root.title("Generator sygnałów")
-root.geometry("1170x550")
+root.geometry("1170x650")
 root.resizable(False, False)
 
 ttk.Label(root, text="Typ sygnału:").grid(row=0, column=0, padx=5, pady=5)
@@ -271,12 +394,11 @@ bins_label.pack(side=tk.LEFT, padx=5)
 increase_button = ttk.Button(bins_frame, text="+", command=increase_bins)
 increase_button.pack(side=tk.LEFT)
 
+generate_frame = ttk.Frame(root)
+generate_frame.grid(row=8, column=0, columnspan=2, pady=10)
 
-generate_button = ttk.Button(root, text="Generuj sygnał", command=generate_signal)
-generate_button.grid(row=8, column=0, padx=5, pady=5)
-
-generate_button = ttk.Button(root, text="Generuj histogram", command=plot_histogram)
-generate_button.grid(row=8, column=1, padx=5, pady=5)
+ttk.Button(generate_frame, text="Generuj sygnał", command=lambda: generate_signal()).pack(side=tk.LEFT, padx=5)
+ttk.Button(generate_frame, text="Generuj histogram", command=lambda: histogram_managment()).pack(side=tk.LEFT, padx=5)
 
 signal_notebook = ttk.Notebook(root)
 signal_notebook.grid(row=0, column=2, rowspan=10, columnspan=10, padx=10, pady=10)
@@ -326,6 +448,13 @@ ttk.Button(operations_frame, text="Dodaj", command=lambda: operate_signals("add"
 ttk.Button(operations_frame, text="Odejmij", command=lambda: operate_signals("subtract")).pack(side=tk.LEFT, padx=5)
 ttk.Button(operations_frame, text="Pomnóż", command=lambda: operate_signals("multiply")).pack(side=tk.LEFT, padx=5)
 ttk.Button(operations_frame, text="Podziel", command=lambda: operate_signals("divide")).pack(side=tk.LEFT, padx=5)
+
+dao_frame = ttk.Frame(root)
+dao_frame.grid(row=10, column=0, columnspan=2, pady=10)
+
+ttk.Button(dao_frame, text="Zapisz sygnał", command=lambda: save_signal()).pack(side=tk.LEFT, padx=5)
+ttk.Button(dao_frame, text="Wczytaj sygnał", command=lambda: load_signal(0)).pack(side=tk.LEFT, padx=5)
+ttk.Button(dao_frame, text="Wyświetl dane", command=lambda: load_signal(1)).pack(side=tk.LEFT, padx=5)
 
 toggle_fields()
 
