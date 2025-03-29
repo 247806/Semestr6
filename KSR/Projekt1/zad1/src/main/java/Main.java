1b6501e3258e3d30251baf156576ff91d72afc55
CountToFour <247806@edu.p.lodz.pl>
2025-03-29 01:13:30 +0100

    KSR -> set proportion

--- KSR/Projekt1/zad1/src/main/java/Main.java
+++ KSR/Projekt1/zad1/src/main/java/Main.java
@@ -1,16 +1,20 @@
 import extraction.FeatureExtractor;
 import extraction.Normalization;
-import extraction.Stemmer;
 import loading.Article;
 
 import java.io.IOException;
 import java.util.*;
 
 import static extraction.StopListFilter.loadStopList;
-import static extraction.StopListFilter.removeStopWords;
 import static loading.Load.loadReutersArticles;
 
 public class Main {
+    private static final List<String> TARGET_COUNTRIES = List.of("japan", "usa", "uk", "canada", "west-germany", "france");
+    private static final int MIN_ARTICLES = 180;
+    private static final int MAX_ARTICLES = 220;
+    private static final int K = 10;
+    private static final double SET_PROPORTION = 0.6;
+
     public static void main(String[] args) throws IOException {
         String datasetPath = "src/main/resources/articles";
         List<String> filePaths = new ArrayList<>();
@@ -27,47 +31,40 @@ public class Main {
         List<Map<String, Object>> testFeatures = new ArrayList<>();
 
         try {
-            List<Article> trainArticles = loadReutersArticles(datasetPath, "TRAIN");
+            List<Article> loadedTrainArticles = loadReutersArticles(datasetPath, "TRAIN");
+            List<Article> trainArticles = selectBalancedTrainSet(loadedTrainArticles);
             System.out.println("Załadowano " + trainArticles.size() + " artykułów trenujących.");
-            List<Article> testArticles = loadReutersArticles(datasetPath, "TEST");
+
+            List<Article> loadedTestArticles = loadReutersArticles(datasetPath, "TEST");
+            List<Article> testArticles = calculateTestArticleNumber(trainArticles.size(), loadedTestArticles);
             System.out.println("Załadowano " + testArticles.size() + " artykułów testowych.");
+
             Map<String, Object> features;
+            countCountries(trainArticles);
 //            String result = stemmer.stem("I'm testing word Japanese");
 //            System.out.println(result);
-            int counter = 1;
-            for (Article article : trainArticles) {
-                System.out.println(counter++ + " / " + trainArticles.size());
-                removeStopWords(article.getBody(), stopWords);
-                features = featureExtractor.extractFeatures(article.getBody());
-                trainFeatures.add(features);
-                article.setFeatures(features);
-            }
-            counter = 1;
-            for (Article article : testArticles) {
-                System.out.println(counter++ + " / " + testArticles.size());
-                removeStopWords(article.getBody(), stopWords);
-                features = featureExtractor.extractFeatures(article.getBody());
-                testFeatures.add(features);
-                article.setFeatures(features);
-            }
-
-//            for (int i = 0; i < 3; i++) {
-//                System.out.println(testArticles.get(i).getBody());
-//                String article = removeStopWords("U.S. Dollar, dmk, yen", stopWords);
-//                System.out.println(article);
-//                features.add(featureExtractor.extractFeatures(article));
-//                System.out.println(features.get(i));
+//            int counter = 1;
+//            for (Article article : trainArticles) {
+//                System.out.println(counter++ + " / " + trainArticles.size());
+//                removeStopWords(article.getBody(), stopWords);
+//                features = featureExtractor.extractFeatures(article.getBody());
+//                trainFeatures.add(features);
+//                article.setFeatures(features);
 //            }
-//            System.out.println(Arrays.toString(testArticles.getFirst().getBody().split("\\s+")));
-//            System.out.println(Arrays.toString(featureExtractor.tokenize(testArticles.getFirst().getBody())));
-//            String article = removeStopWords(testArticles.getFirst().getBody(), stopWords);
-//            testFeatures.add(featureExtractor.extractFeatures(article));
-
-            normalization.preprocess(trainFeatures);
-            for (int i = 0; i < 10; i++) {
-                System.out.println(trainFeatures.get(i));
-                System.out.println(normalization.normalize(trainFeatures.get(i)));
-            }
+//            counter = 1;
+//            for (Article article : testArticles) {
+//                System.out.println(counter++ + " / " + testArticles.size());
+//                removeStopWords(article.getBody(), stopWords);
+//                features = featureExtractor.extractFeatures(article.getBody());
+//                testFeatures.add(features);
+//                article.setFeatures(features);
+//            }
+//
+//            normalization.preprocess(trainFeatures);
+//            for (int i = 0; i < 10; i++) {
+//                System.out.println(trainFeatures.get(i));
+//                System.out.println(normalization.normalize(trainFeatures.get(i)));
+//            }
 //
 //            trainFeatures.stream().limit(10).forEach(System.out::println);
 //            testFeatures.stream().limit(10).forEach(System.out::println);
@@ -76,4 +73,72 @@ public class Main {
         }
 
     }
+
+    private static void countCountries(List<Article> articles) {
+        int countJ = 0;
+        int countUSA = 0;
+        int countF = 0;
+        int countC = 0;
+        int countG = 0;
+        int countUK = 0;
+        for (Article a : articles) {
+            if (a.getPlace().contains("japan")) {
+                countJ++;
+            } else if (a.getPlace().contains("usa")) {
+                countUSA++;
+            } else if (a.getPlace().contains("france")) {
+                countF++;
+            } else if (a.getPlace().contains("canada")) {
+                countC++;
+            } else if (a.getPlace().contains("uk")) {
+                countUK++;
+            } else if (a.getPlace().contains("west-germany")) {
+                countG++;
+            }
+        }
+        System.out.println("Japan: " + countJ + " USA: " + countUSA + " Canada: " + countC + " UK: "
+                + countUK + " Germany: " + countG + " France: " + countF);
+    }
+
+    private static List<Article> selectBalancedTrainSet(List<Article> trainArticles) {
+        Map<String, List<Article>> countryArticles = new HashMap<>();
+
+        for (Article article : trainArticles) {
+            for (String country : TARGET_COUNTRIES) {
+                if (article.getPlace().contains(country)) {
+                    countryArticles.putIfAbsent(country, new ArrayList<>());
+                    countryArticles.get(country).add(article);
+                    break;
+                }
+            }
+        }
+
+        List<Article> selectedTrainArticles = new ArrayList<>();
+        Random random = new Random();
+
+        for (String country : TARGET_COUNTRIES) {
+            List<Article> articles = countryArticles.getOrDefault(country, new ArrayList<>());
+
+            if (articles.size() < MIN_ARTICLES) {
+                int size = articles.size();
+                int targetCount = MIN_ARTICLES + random.nextInt(size - MIN_ARTICLES + 1);
+                Collections.shuffle(articles);
+                selectedTrainArticles.addAll(articles.subList(0, Math.min(targetCount, articles.size())));
+            } else {
+                int targetCount = MIN_ARTICLES + random.nextInt(MAX_ARTICLES - MIN_ARTICLES + 1);
+                Collections.shuffle(articles);
+                selectedTrainArticles.addAll(articles.subList(0, Math.min(targetCount, articles.size())));
+            }
+        }
+
+        System.out.println("Wybrano " + selectedTrainArticles.size() + " artykułów do zbioru treningowego.");
+        return selectedTrainArticles;
+    }
+
+    private static List<Article> calculateTestArticleNumber (int y, List<Article> testArticles) {
+        int temp = (int) (y * (1 - Main.SET_PROPORTION) / Main.SET_PROPORTION);
+        Collections.shuffle(testArticles);
+        return new ArrayList<>(testArticles.subList(0, temp));
+    }
+
 }
