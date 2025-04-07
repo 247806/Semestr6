0253a373b79141cb8cb5ef05ef54a1a8de290b6d
247659 <247659@edu.p.lodz.pl>
2025-04-07 16:25:38 +0200

    KSR -> working front

--- KSR/Projekt1/zad1/src/main/java/ksr/HelloController.java
+++ KSR/Projekt1/zad1/src/main/java/ksr/HelloController.java
@@ -1,6 +1,5 @@
 package ksr;
 
-import com.sun.tools.javac.Main;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -11,7 +10,7 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() throws Exception {
+    protected void onHelloButtonClick() throws IOException {
         welcomeText.setText("Welcome to JavaFX Application!");
         Main.main(null);
     }
